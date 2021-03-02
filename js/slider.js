284d13043c976e4a9e0ade75e4a3b8d251b5f65c
Koladele Olaitan <koladeleolaitan@gmail.com>
2021-03-02 15:26:45 +0100

    updates

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,19 @@
+const slides = document.querySelectorAll('.slide');
+const slideCont = document.querySelector('.slide-container');
+
+let i = 0;
+
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.querySelectorAll('.slide');
-  for (i = 0; i < slides.length; i++) {
-      slides[i].classList.toggle('active');
-    if (i === slides.length) {
-        i = 1;
-    }
-  }; 
+  
+  slideCont.style.transform = `translateX(${-i * 33.33}%)`;
+  slideCont.style.transition = `0.5s ease`;
+  i++;
+
+  if(i === slides.length) {
+      i = 0;
+  };
 
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
